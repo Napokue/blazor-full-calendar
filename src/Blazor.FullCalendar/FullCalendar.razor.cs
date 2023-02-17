0c8024d6d288e7f31ce8702814321c1558ab1fe1
Timo de Kort <dekort.timo@gmail.com>
2023-02-17 16:47:03 +0100

    refactor: increase row amount of the calendar to support maximum amount of days

--- src/Blazor.FullCalendar/FullCalendar.razor.cs
+++ src/Blazor.FullCalendar/FullCalendar.razor.cs
@@ -6,8 +6,15 @@ namespace Blazor.FullCalendar;
 public partial class FullCalendar : ComponentBase
 {
     private const DayOfWeek StartingDay = DayOfWeek.Sunday;
+    
+    /// <summary>
+    /// The maximum amount of days that need to be displayed in a
+    /// month are 31 (max amount of days in a month) + 6 (max amount of deviation between the starting day and first day of the month).
+    ///
+    /// With 7 * 6 = 42 displayed days the grid supports displaying this.
+    /// </summary>
     private const byte AmountOfDisplayedDays = 7;
-    private const byte AmountOfDisplayedWeeks = 5;
+    private const byte AmountOfDisplayedWeeks = 6;
     
     private readonly DateTime _today;
     private readonly DateOnly _firstDateOfCalendar;
