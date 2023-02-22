bdeb58be71baad0e7c149c774e7dea9b4f9ae514
Timo de Kort <dekort.timo@gmail.com>
2023-02-22 09:21:24 +0100

    rever: "feat: split calendar menu to its own component"
    This reverts commit 738b587ac5fb06851e3110e0498b075ef59f5436.

--- src/Blazor.FullCalendar/Models/Constants.cs
+++ src/Blazor.FullCalendar/Models/Constants.cs
@@ -2,14 +2,5 @@
 
 internal static class Constants
 {
-    public const string NoTitle = "No Title";
-    
-    /// <summary>
-    /// The maximum amount of days that need to be displayed in a
-    /// month are 31 (max amount of days in a month) + 6 (max amount of deviation between the starting day and first day of the month).
-    ///
-    /// With 7 * 6 = 42 displayed days the grid supports displaying this.
-    /// </summary>
-    public const byte AmountOfDisplayedDays = 7;
-    public const byte AmountOfDisplayedWeeks = 6;
+    internal const string NoTitle = "No Title";
 }
