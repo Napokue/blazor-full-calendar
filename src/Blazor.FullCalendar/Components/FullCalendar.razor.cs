738b587ac5fb06851e3110e0498b075ef59f5436
Timo de Kort <dekort.timo@gmail.com>
2023-02-22 09:08:24 +0100

    feat: split calendar menu to its own component

--- src/Blazor.FullCalendar/Components/FullCalendar.razor.cs
+++ src/Blazor.FullCalendar/Components/FullCalendar.razor.cs
@@ -1,5 +1,6 @@
 ﻿using Blazor.FullCalendar.Models;
 using Blazor.FullCalendar.Services;
+using Blazor.FullCalendar.Utils;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
@@ -15,31 +16,23 @@ public partial class FullCalendar<TCalendarEvent> : ComponentBase where TCalenda
     [Parameter] public Func<DateOnly, string>? SetDayDisplay { get; set; }
     [Parameter] public DayOfWeek StartingDay { get; set; } = DayOfWeek.Sunday;
 
-    /// <summary>
-    /// The maximum amount of days that need to be displayed in a
-    /// month are 31 (max amount of days in a month) + 6 (max amount of deviation between the starting day and first day of the month).
-    ///
-    /// With 7 * 6 = 42 displayed days the grid supports displaying this.
-    /// </summary>
-    private const byte AmountOfDisplayedDays = 7;
-    private const byte AmountOfDisplayedWeeks = 6;
-    
-    private readonly DateOnly _today;
-    private DateOnly _firstDateOfCalendar;
-    private DateOnly _firstDayOfMonth;
-    
+    public readonly ComponentSharedInformation ComponentSharedInformation;
+
     private IReadOnlyList<string>? _days;
     private Dictionary<int, TCalendarEvent>? _calendarEvents; 
 
     public FullCalendar()
     {
-        _today = DateOnly.FromDateTime(DateTime.Today);
+        ComponentSharedInformation = new ComponentSharedInformation();
     }
 
     protected override void OnInitialized()
     {
+        ComponentSharedInformation.FirstDateOfCalendar = CalculateFirstDateOfCalendar(ComponentSharedInformation.Today);
+        
+        // TODO Review how this can be improved, StateHasChanged is called too much
+        ComponentSharedInformation.PropertyChanged += (_, _) => StateHasChanged();
         _calendarEvents = new Dictionary<int, TCalendarEvent>();
-        _firstDateOfCalendar = CalculateFirstDateOfCalendar(_today);
         _days = WeekService.GetDaysBasedOnStartingDay(StartingDay);
     }
 
@@ -49,7 +42,8 @@ public partial class FullCalendar<TCalendarEvent> : ComponentBase where TCalenda
     /// <param name="week">Current displayed week.</param>
     /// <param name="day">Current displayed day.</param>
     /// <returns>Returns the day based on the displayed week and day.</returns>
-    private DateOnly CalculateDay(int week, int day) => _firstDateOfCalendar.AddDays(week * AmountOfDisplayedDays + day);
+    private DateOnly CalculateDay(int week, int day) =>
+        ComponentSharedInformation.FirstDateOfCalendar.AddDays(week * Constants.AmountOfDisplayedDays + day);
     
     /// <summary>
     /// Calculates first date of the calendar based on the configured starting day of the week. 
@@ -58,66 +52,36 @@ public partial class FullCalendar<TCalendarEvent> : ComponentBase where TCalenda
     /// <returns>Returns the first date of the calendar.</returns>
     private DateOnly CalculateFirstDateOfCalendar(DateOnly date)
     {
-        _firstDayOfMonth = FirstDayOfMonth(date);
-        return _firstDayOfMonth.AddDays(0 - CalculateDayDifference(_firstDayOfMonth.DayOfWeek, StartingDay));
+        ComponentSharedInformation.FirstDayOfMonth = DateUtils.FirstDayOfMonth(date);
+        var dayDifference = DateUtils.CalculateDayDifference(
+            ComponentSharedInformation.FirstDayOfMonth
+                .DayOfWeek, StartingDay);
+        return ComponentSharedInformation.FirstDayOfMonth.AddDays(0 - dayDifference);
     }
-
-    /// <summary>
-    /// Calculates the difference between the starting day of the week and the current day.
-    /// </summary>
-    /// <param name="startingDay">Starting day of the week.</param>
-    /// <param name="currentDay">Current day of the week.</param>
-    /// <returns>Return the difference between the starting and current day of the week.</returns>
-    private static int CalculateDayDifference(DayOfWeek currentDay, DayOfWeek startingDay)
-    {
-        var difference = (int) currentDay - (int) startingDay;
-
-        // If the difference is negative, this means the current day is not in the week of the starting day, but a week before.
-        if (difference < 0)
-        {
-            difference += AmountOfDisplayedDays;
-        }
-
-        return Math.Abs(difference);
-    }
-
-    /// <summary>
-    /// Calculates the first <see cref="DateOnly"/> of the month based on the input <see cref="DateOnly"/>.  
-    /// </summary>
-    /// <param name="date"><see cref="DateOnly"/> that is used to calculate the first <see cref="DateOnly"/> of the month with.</param>
-    /// <returns>Returns first <see cref="DateOnly"/> of the month.</returns>
-    private static DateOnly FirstDayOfMonth(DateOnly date) => date.AddDays(0 - (date.Day - 1));
     
     private void OnWheel(WheelEventArgs e)
     {
         // Scrolling up
         if (e.DeltaY < 0)
         {
-            var date = _firstDayOfMonth.AddMonths(-1);
-            _firstDateOfCalendar = CalculateFirstDateOfCalendar(date);
+            var date = ComponentSharedInformation.FirstDayOfMonth.AddMonths(-1);
+            ComponentSharedInformation.FirstDateOfCalendar = CalculateFirstDateOfCalendar(date);
         }
 
         // Scrolling down
         if (e.DeltaY > 0)
         {
-            var date = _firstDayOfMonth.AddMonths(1);
-            _firstDateOfCalendar = CalculateFirstDateOfCalendar(date);
+            var date = ComponentSharedInformation.FirstDayOfMonth.AddMonths(1);
+            ComponentSharedInformation.FirstDateOfCalendar = CalculateFirstDateOfCalendar(date);
         }
     }
     
-    private void NavigateToToday()
-    {
-        _firstDateOfCalendar = CalculateFirstDateOfCalendar(_today);
-    }
-
     private void OnDayClick(DateOnly date)
     {
         _calendarEvents.TryAdd(date.DayNumber, (TCalendarEvent) CalendarEventFactory.Create(date));
     }
 
-    private string SetMenuDateDisplayInternal() => SetMenuDateDisplay != null
-        ? SetMenuDateDisplay(_firstDayOfMonth)
-        : _firstDayOfMonth.ToString("MMMM yyyy");
+    
 
     private string SetFirstDayOfMonthDisplayInternal(DateOnly day) => SetFirstDayOfMonthDisplay != null
         ? SetFirstDayOfMonthDisplay(day)
